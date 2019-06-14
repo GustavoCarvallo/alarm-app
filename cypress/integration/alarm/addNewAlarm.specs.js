fbba9c3de9bd51c427a3b82b8d4e9332cb20a7bc
Gustavo <gustavo.carvallo@ing.austral.edu.ar>
2019-06-14 04:35:15 -0300

    Add a new separate e2e test for alarm addition

--- cypress/integration/alarm/addNewAlarm.specs.js
+++ cypress/integration/alarm/addNewAlarm.specs.js
@@ -1,8 +1,7 @@
-const nextDateAlarm ='2019-06-14T13:00';
-const nextDateAlarm2 ='2019-06-15T13:00';
+const nextDateAlarm ='2019-06-15T13:00';
 const pastDateAlarm ='2019-06-13T15:00';
 
-describe("Add a new alarm successfully", () => {
+describe("Add alarm functionality", () => {
   before(() => {
     //Go to alarm page
     cy.visit('/');
@@ -13,9 +12,15 @@ describe("Add a new alarm successfully", () => {
     cy.get('[data-cy=add-new-alarm-btn]').click();
     cy.get('[data-cy=modal]').should('exist');
     cy.wait(1000);
+
+    //Close the modal
+    cy.get('[data-cy=modal-cancel-btn').click();
   });
 
   it('If a date is enter it should enable the accept button', () => {
+    //Open the modal
+    cy.get('[data-cy=add-new-alarm-btn]').click();
+    cy.wait(1000);
 
     //Select the input and enter the date
     cy.get('[data-cy="modal-date-input"]').then(input => {
@@ -32,11 +37,10 @@ describe("Add a new alarm successfully", () => {
 
     //Close the modal
     cy.get('[data-cy=modal-cancel-btn').click();
-
   });
 
   it('If no date is enter it should disable the accept button ', () => {
-    //Click the add button
+    //Open the modal
     cy.get('[data-cy=add-new-alarm-btn]').click();
     cy.wait(1000);
 
@@ -48,7 +52,30 @@ describe("Add a new alarm successfully", () => {
     cy.get('[data-cy=modal-cancel-btn').click();
   });
 
+  it('If a past date is enter it should not allow adding the alarm', () => {
+    //Open the modal
+    cy.get('[data-cy=add-new-alarm-btn]').click();
+    cy.wait(1000);
+
+    //Select the input and enter a past date
+    cy.get('[data-cy="modal-date-input"]').then(input => {
+      cy.wait(1000);
+      input[0].dispatchEvent(new Event('input', { bubbles: true }));
+      input.val(pastDateAlarm)
+    });
+    cy.get('[data-cy="modal-date-input"]').trigger('change');
+    cy.wait(1000);
+
+    //Checks the accept button
+    cy.get('[data-cy="modal-accept-btn"]').should('be.disabled');
+  });
+
   it('Add a new alarm should be listed on the alarm`s list', () => {
+    //Close the modal
+    cy.get('[data-cy=modal-cancel-btn').click();
+    cy.wait(1000);
+
+    //Open the modal
     cy.get('[data-cy=add-new-alarm-btn]').click();
 
     //Select the input and enter the date
@@ -65,95 +92,7 @@ describe("Add a new alarm successfully", () => {
     cy.wait(1000);
 
     //Checks if the new alarm is show on the list.
-    cy.get('.mat-row .ng-star-inserted').contains('14/06/2019 - 1:00 PM');
-  });
-
-  it('Enter a past date should not allow to add the alarm', () => {
-    //Click the add button
-    cy.get('[data-cy=add-new-alarm-btn]').click();
-    cy.wait(1000);
-
-    //Select the input and enter the date
-    cy.get('[data-cy="modal-date-input"]').then(input => {
-      cy.wait(1000);
-      input[0].dispatchEvent(new Event('input', { bubbles: true }));
-      input.val(pastDateAlarm)
-    });
-    cy.get('[data-cy="modal-date-input"]').trigger('change');
-    cy.wait(1000);
-
-    //Checks the accept button
-    cy.get('[data-cy="modal-accept-btn"]').should('be.disabled');
-  });
-
-  it('Click the edit alarm button should open a modal to edit the alarm', () => {
-    //Close the modal
-    cy.get('[data-cy=modal-cancel-btn').click();
-
-    cy.get('[data-cy=edit-alarm]').click();
-    cy.get('[data-cy=modal]').should('exist');
-    cy.wait(1000);
-  });
-
-  it('If a date is enter it should enable the accept button ', () => {
-
-    //Select the input and enter the date
-    cy.get('[data-cy="modal-date-input"]').then(input => {
-      cy.wait(1000);
-      input[0].dispatchEvent(new Event('input', { bubbles: true }));
-      input.val(nextDateAlarm2)
-    });
-    cy.get('[data-cy="modal-date-input"]').trigger('change');
-    cy.wait(1000);
-
-    //Check the accept button
-    cy.get('[data-cy="modal-accept-btn"]').should('be.enabled');
-    cy.wait(1000);
-
-    //Close the modal
-    cy.get('[data-cy=modal-cancel-btn').click();
-
-  });
-
-
-  it('If the alarm is edited, it should be reflect on the alarm`s list ', () => {
-
-    //Open the edit modal
-    cy.get('[data-cy=edit-alarm]').click();
-
-    //Select the input and enter no date
-    cy.get('[data-cy="modal-date-input"]').then(input => {
-      cy.wait(1000);
-      input[0].dispatchEvent(new Event('input', { bubbles: true }));
-      input.val(nextDateAlarm2)
-    });
-    cy.get('[data-cy="modal-date-input"]').trigger('change');
-    cy.wait(1000);
-
-    //Click the accept button
-    cy.get('[data-cy="modal-accept-btn"]').click();
-    cy.wait(2000);
-
-    //Checks if the alarm was edited on the list.
     cy.get('.mat-row .ng-star-inserted').contains('15/06/2019 - 1:00 PM');
-
-  });
-
-  it('If the delete button is click a modal should open', () => {
-    // //Close the modal
-    // cy.get('[data-cy=modal-cancel-btn').click();
-
-    cy.get('[data-cy=delete-alarm]').click();
-    cy.get('[data-cy=modal]').should('exist');
-    cy.wait(1000);
-  });
-
-  it('If click the accept button to delete the alarm, the alarm should be deleted', () => {
-
-    cy.get('[data-cy=modal-accept-btn]').click();
-    cy.wait(4000);
-    cy.get('.mat-row .ng-star-inserted').should('not.exist');
-
   });
 
 });
